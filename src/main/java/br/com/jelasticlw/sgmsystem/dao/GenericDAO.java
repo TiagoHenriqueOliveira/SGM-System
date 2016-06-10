16fda57be44f5b1ebca0a83d508c5f2098d4695a
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-10 20:23:06 -0300

    Adicionado interface com os metodos

---
+++ src/main/java/br/com/jelasticlw/sgmsystem/dao/GenericDAO.java
@@ -0,0 +1,15 @@
+package br.com.jelasticlw.sgmsystem.dao;
+
+import java.util.List;
+
+import br.com.jelasticlw.sgmsystem.model.Entidade;
+
+public interface GenericDAO<T extends Entidade> {
+	
+	void salvar(T entidade);
+	void excluir(T entidade);
+	T buscarPorCodigo(Class<T> classe, Long codigo);
+	List<T> buscarPorDescricao(Class<T> classe, String descricao);
+	List<T> buscarTodos(Class<T> classe);
+	
+}
