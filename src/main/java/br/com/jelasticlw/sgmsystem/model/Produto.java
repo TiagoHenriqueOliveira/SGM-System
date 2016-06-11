46d2372fcbb5cc1b4b15f3e93624893d6fd72079
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-10 22:20:03 -0300

    Criado query para: pesquisar por descricao e listar todos

--- src/main/java/br/com/jelasticlw/sgmsystem/model/Produto.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/model/Produto.java
@@ -9,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,14 +21,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="TB_PRODUTO")
+@NamedQueries({
+	@NamedQuery(name = Produto.PesquisaPorDescricao,
+			query = "from tb_produto tbProd "
+					+ "inner join tb_unidade_medida tbUnid "
+					+ "on tbProd.id_unidade_medida = tbUnid.id_unidade_medida "
+					+ "where tbProd.nome like ? "
+					+ "order by tbProd.nome"),
+	@NamedQuery(name = Produto.ListarTodos,
+			query = "from tb_produto tbProd "
+					+ "inner join tb_unidade_medida tbUnid "
+					+ "on tbProd.id_unidade_medida = tbUnid.id_unidade_medida "
+					+ "order by tbProd.nome")
+})
 public @Data class Produto implements Entidade {
+	
+	public static final String PesquisaPorDescricao = "PesquisaPorDescricao";
+	public static final String ListarTodos = "ListarTodos";
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_produto")
 	private Long codigo;
 	
 	@Column(nullable = false, length = 255, unique = true)
-	private String descricao;
+	private String nome;
 	
 	@Column(nullable = false, precision = 10, scale = 2)
 	private Double valor;
@@ -35,6 +54,7 @@ public @Data class Produto implements Entidade {
 	private Boolean foraDeUso;
 	
 	@ManyToOne(optional = true, fetch = FetchType.EAGER)
+	@Column(name = "id_unidade_medida")
 	private UnidadeMedida unidade;
 	
 	@Enumerated(EnumType.STRING)
