559812c4e5e5323d8b603212dad6d62b1a1cd899
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-10 21:34:23 -0300

    Criado query para pesquisar por descricao e listar todos

--- src/main/java/br/com/jelasticlw/sgmsystem/model/UnidadeMedida.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/model/UnidadeMedida.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -14,16 +16,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="TB_UNIDDADE_MEDIDA")
+@Table(name="TB_UNIDADE_MEDIDA")
+@NamedQueries({
+	@NamedQuery(name = UnidadeMedida.PesquisaPorDescricao,
+			query = "from tb_unidade_medida tbUnid "
+					+ "where tbUnid.nome like ?"),
+	@NamedQuery(name = UnidadeMedida.ListarTodos,
+			query = "from tb_unidade_medida ")
+})
 public @Data class UnidadeMedida implements Entidade {
+	
+	public static final String PesquisaPorDescricao = "PesquisaPorDescricao";
+	public static final String ListarTodos = "ListarTodos";
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_unidade_medida")
 	private Long codigo;
 	
 	@Column(nullable = false, length = 255, unique = true)
-	private String descricao;
+	private String nome;
 	
-	@Column(nullable = false, length = 255, unique = true)
+	@Column(nullable = false, length = 3, unique = true)
 	private String abreviatura;
 }
