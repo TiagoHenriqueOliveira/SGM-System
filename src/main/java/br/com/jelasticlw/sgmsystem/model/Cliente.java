b0b7a26c4168a57284087ceb7aa465622645a5ca
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-11 14:46:53 -0300

    Criado query para: pesquisa por nome, cpf e listar todos

--- src/main/java/br/com/jelasticlw/sgmsystem/model/Cliente.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/model/Cliente.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -17,10 +19,45 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="TB_CLIENTE")
+@NamedQueries({
+	@NamedQuery(name = Cliente.PesquisaPorDescricao,
+			query = "from tb_cliente tbCli "
+					+ "inner join tb_cidade tbCid "
+					+ "on tbCli.id_cidade = tbCid.id_cidade "
+					+ "inner join tb_uf tbUF "
+					+ "on tbCid.id_uf = tbUF.id_uf "
+					+ "inner join tb_pais tbPais "
+					+ "on tbUF.id_pais = tbPais.id_pais "
+					+ "where tbCli.nome like ? "
+					+ "order by tbCli.nome"),
+	@NamedQuery(name = Cliente.PesquisaPorCpf,
+			query = "from tb_cliente tbCli "
+					+ "inner join tb_cidade tbCid "
+					+ "on tbCli.id_cidade = tbCid.id_cidade "
+					+ "inner join tb_uf tbUF "
+					+ "on tbCid.id_uf = tbUF.id_uf "
+					+ "inner join tb_pais tbPais "
+					+ "on tbUF.id_pais = tbPais.id_pais "
+					+ "where tbCli.cpf = ? "),
+	@NamedQuery(name = Cliente.ListarTodos,
+			query = "from tb_cliente tbCli "
+					+ "inner join tb_cidade tbCid "
+					+ "on tbCli.id_cidade = tbCid.id_cidade "
+					+ "inner join tb_uf tbUF "
+					+ "on tbCid.id_uf = tbUF.id_uf "
+					+ "inner join tb_pais tbPais "
+					+ "on tbUF.id_pais = tbPais.id_pais "
+					+ "order by tbCli.nome")
+})
 public @Data class Cliente implements Entidade {
+	
+	public static final String PesquisaPorDescricao = "PesquisaPorDescricao";
+	public static final String PesquisaPorCpf = "PesquisaPorCpf";
+	public static final String ListarTodos = "PesquisaTodos";
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
 	private Long codigo;
 	
 	@Column(nullable = false, length = 255)
@@ -51,5 +88,6 @@ public @Data class Cliente implements Entidade {
 	private Boolean foraDeUso;
 	
 	@ManyToOne(optional = true, fetch = FetchType.EAGER)
+	@Column(name = "id_cidade")
 	private Cidade cidade;
 }
