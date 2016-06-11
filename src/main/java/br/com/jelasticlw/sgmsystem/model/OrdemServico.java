6281aa2053156d81c1d5fd37ee940c831d4091b8
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-11 15:39:52 -0300

    Criado query para: buscar por nome do cliente, por placa do veiculo e para listar todos

--- src/main/java/br/com/jelasticlw/sgmsystem/model/OrdemServico.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/model/OrdemServico.java
@@ -12,6 +12,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -26,10 +28,39 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="TB_ORDEM_SERVICO")
+@NamedQueries({
+	@NamedQuery(name = OrdemServico.PesquisaPorDescricao,
+			query = "from tb_ordem_servico tbOSV "
+					+ "inner join tb_veiculo tbVeiculo "
+					+ "on tbOSV.id_veiculo = tbVeiculo.id_veiculo "
+					+ "inner join tb_cliente tbCli "
+					+ "on tbVeiculo.id_cliente = tbCli.id_cliente "
+					+ "where tbCli.nome like ? "
+					+ "order by tbCli.nome"),
+	@NamedQuery(name = OrdemServico.PesquisaPorPlaca,
+			query = "from tb_ordem_servico tbOSV "
+					+ "inner join tb_veiculo tbVeiculo "
+					+ "on tbOSV.id_veiculo = tbVeiculo.id_veiculo "
+					+ "inner join tb_cliente tbCli "
+					+ "on tbVeiculo.id_cliente = tbCli.id_cliente "
+					+ "where tbVeiculo.placa = ? "),
+	@NamedQuery(name = OrdemServico.ListarTodos,
+			query = "from tb_ordem_servico tbOSV "
+					+ "inner join tb_veiculo tbVeiculo "
+					+ "on tbOSV.id_veiculo = tbVeiculo.id_veiculo "
+					+ "inner join tb_cliente tbCli "
+					+ "on tbVeiculo.id_cliente = tbCli.id_cliente "
+					+ "order by tbCli.nome")
+})
 public @Data class OrdemServico implements Entidade {
+	
+	public static final String PesquisaPorDescricao = "PesquisaPorDescricao";
+	public static final String PesquisaPorPlaca = "PesquisaPorPlaca";
+	public static final String ListarTodos = "ListarTodos";
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_ordem_servico")
 	private Long codigo;
 	
 	@Column(nullable = false, length = 11)
@@ -40,12 +71,11 @@ public @Data class OrdemServico implements Entidade {
 	private Date dataAgendamento;
 	
 	@ManyToOne(optional = true, fetch = FetchType.EAGER)
-	private Cliente cliente;
-	
-	@ManyToOne(optional = true, fetch = FetchType.EAGER)
+	@Column(name = "id_veiculo")
 	private Veiculo veiculo;
 	
 	@OneToOne(optional = true, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@Column(name = "id_servico")
 	private Servico servico;
 	
 	@OneToMany(mappedBy = "OSVItemProduto", targetEntity = OSVItemProduto.class, fetch=FetchType.EAGER, cascade = CascadeType.ALL)
