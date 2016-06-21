2364bc3d743fcd900885e46286a331654c4ba571
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-20 21:28:13 -0300

    Alterado nome da classe UnidadeMedida para Undiade

---
+++ src/main/java/br/com/jelasticlw/sgmsystem/model/Unidade.java
@@ -0,0 +1,30 @@
+package br.com.jelasticlw.sgmsystem.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="TB_UNIDADE_MEDIDA")
+public @Data class Unidade implements Entidade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@Column(nullable = false, length = 255, unique = true)
+	private String nome;
+	
+	@Column(nullable = false, length = 3, unique = true)
+	private String abreviatura;
+
+}
