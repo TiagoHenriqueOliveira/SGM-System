598c14bcdeb2948d2f375a013862c98c3c3f64d4
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-20 23:28:54 -0300

    Ajustado imports do clienteDAO

--- src/main/java/br/com/jelasticlw/sgmsystem/dao/ClienteDAO.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/dao/ClienteDAO.java
@@ -1,12 +1,10 @@
 package br.com.jelasticlw.sgmsystem.dao;
 
 import javax.enterprise.context.RequestScoped;
-import javax.persistence.NoResultException;
-import javax.persistence.TypedQuery;
 
 import br.com.jelasticlw.sgmsystem.model.Cliente;
-import br.com.jelasticlw.sgmsystem.model.Usuario;
 
 @RequestScoped
 public class ClienteDAO extends HibernateDAO<Cliente> {
+	
 }
