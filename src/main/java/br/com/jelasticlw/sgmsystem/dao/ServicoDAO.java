cb4133cb198d26e57c95e8ad074114533faaf621
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-22 13:04:41 -0300

    Adicionado ServicoDAO herdando o HibernateDAO

---
+++ src/main/java/br/com/jelasticlw/sgmsystem/dao/ServicoDAO.java
@@ -0,0 +1,10 @@
+package br.com.jelasticlw.sgmsystem.dao;
+
+import javax.enterprise.context.RequestScoped;
+
+import br.com.jelasticlw.sgmsystem.model.Servico;
+
+@RequestScoped
+public class ServicoDAO extends HibernateDAO<Servico> {
+
+}
