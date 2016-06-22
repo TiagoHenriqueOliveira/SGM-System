c11ea2593400bcc2c85d0f88d344d3de2d723cb1
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-22 20:54:03 -0300

    Corrigido metodo login

--- src/main/java/br/com/jelasticlw/sgmsystem/dao/UsuarioDAO.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/dao/UsuarioDAO.java
@@ -9,12 +9,12 @@ import br.com.jelasticlw.sgmsystem.model.Usuario;
 @RequestScoped
 public class UsuarioDAO extends HibernateDAO<Usuario> {
 
-	public Usuario login(String login, String senha) {
+	public Usuario login(Usuario usuario) {
 		super.conectar();
 		try {
 			TypedQuery<Usuario> query = em.createNamedQuery(Usuario.Login, Usuario.class);
-			query.setParameter("login", login);
-			query.setParameter("senha", senha);
+			query.setParameter("login", usuario.getLogin());
+			query.setParameter("senha", usuario.getSenha());
 			return query.getSingleResult();
 		} catch (NoResultException e) {
 			return null;
