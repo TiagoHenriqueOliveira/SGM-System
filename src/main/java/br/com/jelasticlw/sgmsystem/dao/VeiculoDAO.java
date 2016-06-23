35aedf1802a91fe310fc71eaa597b0d42e2b6ba5
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-23 19:44:53 -0300

    Criado query para listar os veiculos do cliente

--- src/main/java/br/com/jelasticlw/sgmsystem/dao/VeiculoDAO.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/dao/VeiculoDAO.java
@@ -1,10 +1,24 @@
 package br.com.jelasticlw.sgmsystem.dao;
 
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.TypedQuery;
 
 import br.com.jelasticlw.sgmsystem.model.Veiculo;
 
 @RequestScoped
 public class VeiculoDAO extends HibernateDAO<Veiculo> {
 	
+	public List<Veiculo> listaVeiculosCliente(Class<Veiculo> veiculo, Long codigo) {
+		super.conectar();
+		try {
+			String hql = "from Veiculo v where v.cliente.codigo = ?";
+			TypedQuery<Veiculo> query = em.createQuery(hql, Veiculo.class);
+			query.setParameter(1, codigo);
+			return query.getResultList();
+		} finally {
+			super.desconectar();
+		}
+	}
 }
