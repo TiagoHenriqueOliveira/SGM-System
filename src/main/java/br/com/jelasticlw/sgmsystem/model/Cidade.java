ca14aababd26e02d8b2d9c5ea77dadb2d9cb564e
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-23 14:53:17 -0300

    Removido query

--- src/main/java/br/com/jelasticlw/sgmsystem/model/Cidade.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/model/Cidade.java
@@ -8,8 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,16 +17,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="TB_CIDADE")
-@NamedQueries({
-	@NamedQuery(name = Cidade.ListaCidadesPorEstado,
-			query = "from Cidade c "
-					+ "where c.uf.codigo = :id_uf "
-					+ "order by c.nome")
-})
 public @Data class Cidade implements Entidade {
 
-	public static final String ListaCidadesPorEstado = "ListaCidadesPorEstado";
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long codigo;
