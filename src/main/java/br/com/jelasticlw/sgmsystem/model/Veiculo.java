a2bd1bc1f4f18d6cffc4bf425cdd9e213f339423
Tiago <tiago_murtosa@yahoo.com.br>
2016-06-23 13:39:54 -0300

    Identado código

--- src/main/java/br/com/jelasticlw/sgmsystem/model/Veiculo.java
+++ src/main/java/br/com/jelasticlw/sgmsystem/model/Veiculo.java
@@ -8,30 +8,28 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="TB_VEICULO")
+@Table(name = "TB_VEICULO")
 public @Data class Veiculo implements Entidade {
-	
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
-	
+
 	@Column(nullable = false, length = 255)
 	private String nome;
-	
+
 	@Column(nullable = false, length = 255)
 	private String placa;
-	
+
 	@ManyToOne(optional = true, fetch = FetchType.EAGER)
 	@JoinColumn(name = "id_cliente")
 	private Cliente cliente;
